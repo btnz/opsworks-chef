ace880a1fb604d9f15419c922b1f71d8979927c9
Bevan Thomson <bevan@bevanthomson.net>
2014-07-19 06:24:32 +0100

    Initial upload

---
+++ nginx-django/metadata.rb
@@ -0,0 +1,9 @@
+name             'nginx-django'
+maintainer       'Keteparaha Ltd'
+maintainer_email 'bevan@keteparaha.co.uk'
+license          'All rights reserved'
+description      'Installs/Configures nginx-django'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "nginx"
