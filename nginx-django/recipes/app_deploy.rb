32824abb09367713c8f9242f18de8e9a7a39db48
Bevan Thomson <bevan@bevanthomson.net>
2014-07-19 09:19:01 +0100

    New appl deploy recipe

---
+++ nginx-django/recipes/app_deploy.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: nginx-django
+# Recipe:: app_deploy
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'deploy'
+
+node[:deploy].each.do | application, deploy|
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+opsworks_deploy do
+  deploy_data deploy
+  app webApp1
+end
+
